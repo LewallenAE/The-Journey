7b4d0d7ac12ca8f85a583a22078ce5835a9a286e
Anthony Lewallen <lewallenae@gmail.com>
2025-07-21 16:32:48 -0700

    Adding files

--- FrontEndMasters/01 Beginner/03_Getting_Started_with_JavaScript_V2/12 Three_Pillars_of_JS_Exercise.js
+++ FrontEndMasters/01 Beginner/03_Getting_Started_with_JavaScript_V2/12 Three_Pillars_of_JS_Exercise.js
@@ -1,4 +1,29 @@
-class Bookshelf(param1, param2) {
+class Bookshelf {
+    constructor() {
+        this.bookList = [];
+    }
 
-    
+    addFavoriteBook(bookName) {
+        if (!bookName.includes("Great")) {
+            this.bookList.push(bookName);
+        }
+    }
+
+    printFavoriteBooks() {
+        for (let i = 0; i < this.bookList.length; i++) {
+            console.log(`Favorite book: ${i + 1} ${this.bookList[i]}`)
+        }
+    }    
 }
+
+const myBookShelf = new Bookshelf();
+
+myBookShelf.addFavoriteBook("Dune");
+myBookShelf.addFavoriteBook("The Apprentice");
+myBookShelf.addFavoriteBook("You Don't Know JS");
+myBookShelf.addFavoriteBook("The Great Gatsby");
+myBookShelf.addFavoriteBook("Django");
+myBookShelf.addFavoriteBook("Head First Java");
+myBookShelf.addFavoriteBook("The Great White Hype");
+
+myBookShelf.printFavoriteBooks();
